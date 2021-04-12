8fb11ca48802b7b37174f8e546e18771eeb66a98
Nakul Joshi <nakulj@users.noreply.github.com>
2021-04-12 11:41:11 -0700

    Use standard layout.

--- src/3/main.rs
+++
@@ -1,4 +0,0 @@
-mod primes;
-fn main() {
-  println!("{}", primes::largest_prime_factor(600851475143));
-}
---
+++ src/bin/euler3.rs
@@ -0,0 +1,4 @@
+use euler_rs::primes::largest_prime_factor;
+fn main() {
+  println!("{}",largest_prime_factor(600851475143));
+}
